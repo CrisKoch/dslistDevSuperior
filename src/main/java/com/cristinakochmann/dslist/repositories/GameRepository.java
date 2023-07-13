4883c82d409ab2904eb95f601dc718bd9ba443cb
Cristina Kochmann <cristinak.iftm@gmail.com>
2023-07-13 10:44:46 -0300

    Game repository, service, controller

---
+++ src/main/java/com/cristinakochmann/dslist/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.cristinakochmann.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cristinakochmann.dslist.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+}
